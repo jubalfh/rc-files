7fc61a45189d5b200c72cc59509876c6f7319980
mawhrin-skel <mawhrin+p+rcfiles@makabra.org>
2022-06-26 10:58:29 +0200

    wezterm: update for readability.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,58 +1,57 @@
-local wezterm = require 'wezterm'
-local act = wezterm.action
+local wezterm = require("wezterm")
+local _a = wezterm.action
 
-local leader = { key="phys:A", mods="CTRL", timeout_milliseconds=1000 }
+local leader = { mods = "CTRL", key = "mapped:`", timeout_milliseconds = 1000 }
 local key_bindings = {
-    {mods="CTRL", key="LeftArrow", action=act{ActivateTabRelative=-1}},
-    {mods="CTRL", key="RightArrow", action=act{ActivateTabRelative=1}},
-    {mods="CTRL|SHIFT", key="LeftArrow", action=act{ActivatePaneDirection="Left"}},
-    {mods="CTRL|SHIFT", key="RightArrow", action=act{ActivatePaneDirection="Right"}},
-    {mods="CTRL|SHIFT", key="UpArrow", action=act{ActivatePaneDirection="Up"}},
-    {mods="CTRL|SHIFT", key="DownArrow", action=act{ActivatePaneDirection="Down"}},
+    { mods = "CTRL", key = "LeftArrow", action = _a.ActivateTabRelative(-1) },
+    { mods = "CTRL", key = "RightArrow", action = _a.ActivateTabRelative(1) },
+    { mods = "CTRL|SHIFT", key = "LeftArrow", action = _a.ActivatePaneDirection("Left") },
+    { mods = "CTRL|SHIFT", key = "RightArrow", action = _a.ActivatePaneDirection("Right") },
+    { mods = "CTRL|SHIFT", key = "UpArrow", action = _a.ActivatePaneDirection("Up") },
+    { mods = "CTRL|SHIFT", key = "DownArrow", action = _a.ActivatePaneDirection("Down") },
 
-    {mods="CTRL|SHIFT", key="r", action="ReloadConfiguration"},
-    {mods="CTRL|SHIFT", key="l", action=act{ClearScrollback = "ScrollbackAndViewport"}},
-    {mods="SHIFT", key="Insert", action=act{PasteFrom="PrimarySelection"}},
+    { mods = "CTRL|SHIFT", key = "r", action = _a.ReloadConfiguration },
+    { mods = "CTRL|SHIFT", key = "l", action = _a.ClearScrollback("ScrollbackAndViewport") },
+    { mods = "SHIFT", key = "Insert", action = _a.PasteFrom("PrimarySelection") },
 
-    {mods="LEADER", key="h", action=act{SplitHorizontal={domain="CurrentPaneDomain"}}},
-    {mods="LEADER", key="v", action=act{SplitVertical={domain="CurrentPaneDomain"}}},
-    {mods="LEADER", key="d", action="ShowDebugOverlay"},
-    {mods="LEADER", key="z", action="TogglePaneZoomState"},
-    {mods="LEADER|CTRL", key="phys:A", action=act{SendString="\x01"}},
+    { mods = "LEADER", key = "h", action = _a.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { mods = "LEADER", key = "v", action = _a.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { mods = "LEADER", key = "d", action = _a.ShowDebugOverlay },
+    { mods = "LEADER", key = "z", action = _a.TogglePaneZoomState },
 }
 
 local mouse_bindings = {
     -- copy to clipboard on ctrl-right-click
-    { event={Down={streak=1, button="Right"}},
-        mods="CTRL",
-        action={CopyTo="ClipboardAndPrimarySelection"}
+    {
+        event = { Down = { streak = 1, button = "Right" } },
+        mods = "CTRL",
+        action = _a.CopyTo("ClipboardAndPrimarySelection"),
     },
     -- paste from clipboard on right-click
-    { event={Down={streak=1, button="Right"}},
-        mods="NONE",
-        action={PasteFrom="Clipboard"}
-    },
+    { event = { Down = { streak = 1, button = "Right" } }, mods = "NONE", action = _a.PasteFrom("Clipboard") },
     -- open url on ctrl-left-click
-    { event={Up={streak=1, button="Left"}},
-        mods="CTRL",
-        action="OpenLinkAtMouseCursor"
-    },
-    { event={Down={streak=1, button="Left"}}, mods="CTRL", action="Nop" },
+    { event = { Up = { streak = 1, button = "Left" } }, mods = "CTRL", action = _a.OpenLinkAtMouseCursor },
+    { event = { Down = { streak = 1, button = "Left" } }, mods = "CTRL", action = _a.Nop },
 }
 
 return {
     -- basic window settings
-    window_close_confirmation="NeverPrompt",
+    window_close_confirmation = "NeverPrompt",
     exit_behavior = "Close",
-    window_padding = {4, 4, 4, 4},
+    window_padding = {
+        left = "4px",
+        right = "4px",
+        top = "4px",
+        bottom = "4px",
+    },
     window_decorations = "RESIZE",
     enable_tab_bar = false,
     -- color scheme
     color_scheme = "Gruvbox Dark",
     -- fonts & input handling
     font = wezterm.font_with_fallback({
-        {family="Iosevka Term SS07", weight="Medium"},
-        {family="Iosevka Term", weight="Medium"},
+        { family = "Iosevka Term SS07", weight = "Medium" },
+        { family = "Iosevka Term", weight = "Medium" },
     }),
     font_size = 12.0,
     allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",

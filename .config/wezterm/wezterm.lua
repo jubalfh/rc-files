49682d2c16ff86c7b301efb928982d0fd06c085c
Miroslaw Baran <miroslaw@makabra.org>
2023-03-25 00:59:06 +0100

    wezterm: convert the config to the new encouraged style.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,77 +1,74 @@
 local wezterm = require("wezterm")
 local _a = wezterm.action
 
-local leader = { mods = "CTRL", key = "mapped:`", timeout_milliseconds = 1000 }
-local key_bindings = {
-    { mods = "CTRL", key = "LeftArrow", action = _a.ActivateTabRelative(-1) },
-    { mods = "CTRL", key = "RightArrow", action = _a.ActivateTabRelative(1) },
-    { mods = "CTRL|SHIFT", key = "LeftArrow", action = _a.ActivatePaneDirection("Left") },
-    { mods = "CTRL|SHIFT", key = "RightArrow", action = _a.ActivatePaneDirection("Right") },
-    { mods = "CTRL|SHIFT", key = "UpArrow", action = _a.ActivatePaneDirection("Up") },
-    { mods = "CTRL|SHIFT", key = "DownArrow", action = _a.ActivatePaneDirection("Down") },
+-- initialize config
+local config = wezterm.config_builder()
 
-    { mods = "CTRL|SHIFT", key = "l", action = _a.ClearScrollback("ScrollbackAndViewport") },
-    { mods = "SHIFT", key = "Insert", action = _a.PasteFrom("PrimarySelection") },
+-- key mappings and leader key
+config.leader = { key = "mapped:`", mods = "CTRL", timeout_milliseconds = 1000 }
+config.keys = {
+    { key = "LeftArrow", mods = "CTRL", action = _a.ActivateTabRelative(-1) },
+    { key = "RightArrow", mods = "CTRL", action = _a.ActivateTabRelative(1) },
+    { key = "LeftArrow", mods = "CTRL|SHIFT", action = _a.ActivatePaneDirection("Left") },
+    { key = "RightArrow", mods = "CTRL|SHIFT", action = _a.ActivatePaneDirection("Right") },
+    { key = "UpArrow", mods = "CTRL|SHIFT", action = _a.ActivatePaneDirection("Up") },
+    { key = "DownArrow", mods = "CTRL|SHIFT", action = _a.ActivatePaneDirection("Down") },
 
-    { mods = "LEADER", key = "h", action = _a.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
-    { mods = "LEADER", key = "v", action = _a.SplitVertical({ domain = "CurrentPaneDomain" }) },
-    { mods = "LEADER", key = "d", action = _a.ShowDebugOverlay },
-    { mods = "LEADER", key = "z", action = _a.TogglePaneZoomState },
-    { mods = "LEADER", key = "r", action = _a.ReloadConfiguration },
+    { key = "l", mods = "CTRL|SHIFT", action = _a.ClearScrollback("ScrollbackAndViewport") },
+    { key = "Insert", mods = "SHIFT", action = _a.PasteFrom("PrimarySelection") },
+
+    { key = "h", mods = "LEADER", action = _a.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+    { key = "v", mods = "LEADER", action = _a.SplitVertical({ domain = "CurrentPaneDomain" }) },
+    { key = "d", mods = "LEADER", action = _a.ShowDebugOverlay },
+    { key = "z", mods = "LEADER", action = _a.TogglePaneZoomState },
+    { key = "r", mods = "LEADER", action = _a.ReloadConfiguration },
 }
 
-local mouse_bindings = {
+-- mouse mappings; simplified (again)
+config.mouse_bindings = {
     -- copy to clipboard on ctrl-right-click
     {
         event = { Down = { streak = 1, button = "Right" } },
         mods = "CTRL",
         action = _a.CopyTo("ClipboardAndPrimarySelection"),
     },
+
     -- paste from clipboard on right-click
     { event = { Down = { streak = 1, button = "Right" } }, mods = "NONE", action = _a.PasteFrom("Clipboard") },
+
     -- open url on ctrl-left-click
     { event = { Up = { streak = 1, button = "Left" } }, mods = "CTRL", action = _a.OpenLinkAtMouseCursor },
     { event = { Down = { streak = 1, button = "Left" } }, mods = "CTRL", action = _a.Nop },
 }
 
-return {
-    -- basic window settings
-    window_close_confirmation = "NeverPrompt",
-    exit_behavior = "Close",
-    window_padding = {
-        left = "4px",
-        right = "4px",
-        top = "4px",
-        bottom = "4px",
-    },
-    window_decorations = "RESIZE",
-    enable_tab_bar = false,
-    -- color scheme
-    color_scheme = "GruvboxDark",
-    -- fonts & input handling
-    font = wezterm.font_with_fallback({
+-- general config data follows
+config.window_close_confirmation = "NeverPrompt"
+config.exit_behavior = "Close"
+config.window_padding = { left="4px", right="4px", top="4px", bottom="4px"}
+config.window_decorations = "RESIZE"
+config.enable_tab_bar = false
+-- color scheme
+config.color_scheme = "GruvboxDark"
+-- fonts & input handling
+config.font = wezterm.font_with_fallback({
         { family = "Iosevka Term SS07", weight = "Medium" },
         { family = "Iosevka Term", weight = "Medium" },
         { family = "Nerd Font Symbol Mono"},
-    }),
-    font_size = 12.0,
-    allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace",
-    adjust_window_size_when_changing_font_size = false,
-    warn_about_missing_glyphs = false,
-    freetype_load_target = "Light",
-    freetype_render_target = "Normal",
-    freetype_interpreter_version = 40,
-    use_ime = true,
-    -- key mappings and leader key
-    leader = leader,
-    keys = key_bindings,
-    -- mouse mappings; simplified (again)
-    mouse_bindings = mouse_bindings,
-    -- no update checks, please
-    check_for_updates = false,
-    -- remaining options
-    scrollback_lines = 25000,
-    -- front_end = "WebGpu",
-    -- webgpu_power_preference = "HighPerformance",
-    animation_fps = 15,
-}
+    })
+config.font_size = 12.0
+config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
+config.adjust_window_size_when_changing_font_size = false
+config.warn_about_missing_glyphs = false
+config.freetype_load_target = "Light"
+config.freetype_render_target = "Normal"
+config.freetype_interpreter_version = 40
+config.use_ime = true
+-- no update checks, please
+config.check_for_updates = false
+-- remaining options
+config.scrollback_lines = 25000
+-- config.front_end = "WebGpu"
+-- config.webgpu_power_preference = "HighPerformance"
+config.animation_fps = 15
+
+return config

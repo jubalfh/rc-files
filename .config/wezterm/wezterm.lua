7fdef06e431ce881bbd257ab6fdac3ee3b8acc56
mawhrin-skel <mawhrin+p+rcfiles@makabra.org>
2023-11-14 13:37:34 +0100

    wezterm: font & click
    * use context-aware right-click copy & paste as seen on windows
      terminal,
    
    * play with some more advanced font settings, and come back to iosevka,
      as usual.
    
    * (also whitespace adjustments ffs).

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -26,15 +26,20 @@ config.keys = {
 
 -- mouse mappings; simplified (again)
 config.mouse_bindings = {
-    -- copy to clipboard on ctrl-right-click
+    -- copy & paste on right-click as seen in windows terminal
+    -- cf. https://github.com/wez/wezterm/discussions/3541
     {
-        event = { Down = { streak = 1, button = "Right" } },
-        mods = "CTRL",
-        action = _a.CopyTo("ClipboardAndPrimarySelection"),
+        event = { Down = { streak = 1, button = "Right" } }, mods = "NONE",
+        action = wezterm.action_callback(function(window, pane)
+            local has_selection = window:get_selection_text_for_pane(pane) ~= ""
+            if has_selection then
+                window:perform_action(_a.CopyTo("ClipboardAndPrimarySelection"), pane)
+                window:perform_action(_a.ClearSelection, pane)
+            else
+                window:perform_action(_a.PasteFrom("Clipboard"), pane)
+            end
+        end),
     },
-
-    -- paste from clipboard on right-click
-    { event = { Down = { streak = 1, button = "Right" } }, mods = "NONE", action = _a.PasteFrom("Clipboard") },
 
     -- open url on ctrl-left-click
     { event = { Up = { streak = 1, button = "Left" } }, mods = "CTRL", action = _a.OpenLinkAtMouseCursor },
@@ -46,28 +51,83 @@ config.mouse_bindings = {
 -- general config data follows
 config.window_close_confirmation = "NeverPrompt"
 config.exit_behavior = "Close"
-config.window_padding = { left="4px", right="4px", top="4px", bottom="4px"}
+config.window_padding = { left = "4px", right = "4px", top = "4px", bottom = "4px" }
 config.window_decorations = "NONE | RESIZE"
 config.enable_tab_bar = false
+
 -- color scheme
 config.color_scheme = "GruvboxDark"
+
 -- fonts & input handling
 config.font = wezterm.font_with_fallback({
-        { family = "PragmataPro Liga" },
-        { family = "Fantasque Sans Mono", weight = "Medium" },
-        { family = "Iosevka Term SS07", weight = "Medium" },
-        { family = "Nerd Font Symbol Mono"},
-    })
-config.font_size = 13.0
-config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
-config.adjust_window_size_when_changing_font_size = false
-config.warn_about_missing_glyphs = false
+    {
+        family = "Iosevka Term",
+        weight = "Regular",
+        harfbuzz_features = { "dlig=1", "ss07=1" },
+    },
+    {
+        family = "PragmataPro Liga",
+    },
+    -- {
+    --     family = "Fantasque Sans Mono", weight = "Regular",
+    -- },
+    -- {
+    --     family = "Victor Mono", weight = "Regular",
+    -- },
+    -- {
+    --     family = "Monaspace Argon", weight = "Light",
+    --     harfbuzz_features = {
+    --         "calt=1", "dlig=1", "liga=1", "ss05=1",
+    --     }
+    -- },
+    {
+        family = "Symbols Nerd Font Mono",
+    },
+})
+config.font_rules = {
+    {
+        intensity = "Bold",
+        italic = true,
+        font = wezterm.font({
+            -- family = Monaspace Radon",
+            family = "Fantasque Sans Mono",
+            weight = "Bold",
+            style = "Italic",
+        }),
+    },
+    {
+        intensity = "Half",
+        italic = true,
+        font = wezterm.font({
+            -- family = Monaspace Radon",
+            family = "Fantasque Sans Mono",
+            weight = "Medium",
+            style = "Italic",
+        }),
+    },
+    {
+        intensity = "Normal",
+        italic = true,
+        font = wezterm.font({
+            -- family = Monaspace Radon",
+            family = "Fantasque Sans Mono",
+            weight = "Regular",
+            style = "Italic",
+        }),
+    },
+}
+config.font_size = 12.5
 config.freetype_load_target = "Light"
 config.freetype_render_target = "Normal"
 config.freetype_interpreter_version = 40
+config.allow_square_glyphs_to_overflow_width = "WhenFollowedBySpace"
+config.adjust_window_size_when_changing_font_size = false
+config.warn_about_missing_glyphs = false
 config.use_ime = true
+
 -- no update checks, please
 config.check_for_updates = false
+
 -- remaining options
 config.scrollback_lines = 25000
 config.front_end = "WebGpu"
